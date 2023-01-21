7c3a8db80ec343f99a3ac9818cf5a272d1df7e40
AkashSingh20 <akashsingh20603@gmail.com>
2023-01-22 02:00:42 +0530

    expansion

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function click1(){
     document.getElementById("but2").style.background="#dcc8ff";
     document.getElementById("but2").style["border-bottom"]="4px solid #4f00dc";
     document.getElementById("but1").style["border-bottom"]="none";
+    document.getElementById("area1").style.display="none";
+    document.getElementById("area2").style.display="flex";
 
     // console.log("clicked");
 }
@@ -12,6 +14,8 @@ function click2(){
     document.getElementById("but1").style.background="#dcc8ff";
     document.getElementById("but1").style["border-bottom"]="4px solid #4f00dc";
     document.getElementById("but2").style["border-bottom"]="none";
+    document.getElementById("area2").style.display="none";
+    document.getElementById("area1").style.display="flex";
 
     // console.log("clicked");
 }
